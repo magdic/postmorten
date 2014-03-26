3bcc7a5a09d010b7c3c0e2e7285f98b03d0c43bf
Magdiel Cordero <contact@magdielcordero.info>
2014-03-26 07:39:57 -0600

    Update dbconfig.php
    Add .gitignore case

--- dbconfig.php
+++ dbconfig.php
@@ -1,8 +1,8 @@
 <?php
 $mysql_hostname = "localhost";
 $mysql_user = "root";
-$mysql_password = "root";
-$mysql_database = "loginTut";
+$mysql_password = "DATABASE";
+$mysql_database = "PASSSWORD";
 $prefix = "";
 $bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
 mysql_select_db($mysql_database, $bd) or die("Could not select database");
