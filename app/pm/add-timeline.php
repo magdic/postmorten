f450129a8a534296d3d1b500319817640fc479f8
Mauricio Ramirez <mau.ram.ro@gmail.com>
2014-04-07 14:28:59 -0600

    logout/add timeline update

--- app/pm/add-timeline.php
+++ app/pm/add-timeline.php
@@ -29,7 +29,7 @@ session_start();
 }
 
 
- 
+
 //Validacion de bandera de error
 $errflag = false;
 //Funcion para recibir valores del form. Previene SQL injection
@@ -50,6 +50,55 @@ function clean($str)
 <html>
 <head>
 	<title>Adding Timelines | my App</title>
+     <script src="//localhost:35729/livereload.js"></script>
+
+     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <!-- basic styles -->
+
+        <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
+        <link rel="stylesheet" href="../assets/css/font-awesome.min.css" />
+
+        <!--[if IE 7]>
+          <link rel="stylesheet" href="assets/css/font-awesome-ie7.min.css" />
+        <![endif]-->
+
+        <!-- page specific plugin styles -->
+
+        <link rel="stylesheet" href="../assets/css/jquery-ui-1.10.3.custom.min.css" />
+        <link rel="stylesheet" href="../assets/css/chosen.css" />
+        <link rel="stylesheet" href="../assets/css/datepicker.css" />
+        <link rel="stylesheet" href="../assets/css/bootstrap-timepicker.css" />
+        <link rel="stylesheet" href="../assets/css/daterangepicker.css" />
+        <link rel="stylesheet" href="../assets/css/colorpicker.css" />
+
+        <!-- fonts -->
+
+        <link rel="stylesheet" href="../assets/css/ace-fonts.css" />
+
+        <!-- ace styles -->
+
+        <link rel="stylesheet" href="../assets/css/ace.min.css" />
+        <link rel="stylesheet" href="../assets/css/ace-rtl.min.css" />
+        <link rel="stylesheet" href="../assets/css/ace-skins.min.css" />
+
+        <!--[if lte IE 8]>
+          <link rel="stylesheet" href="assets/css/ace-ie.min.css" />
+        <![endif]-->
+
+        <!-- inline styles related to this page -->
+
+        <!-- ace settings handler -->
+
+        <script src="../assets/js/ace-extra.min.js"></script>
+
+        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+
+        <!--[if lt IE 9]>
+        <script src="assets/js/html5shiv.js"></script>
+        <script src="assets/js/respond.min.js"></script>
+        <![endif]-->
+
 </head>
 <body>
 	<?php
@@ -70,12 +119,198 @@ function clean($str)
 			//this is out update script which should be used in each page to update the users online time
 			$time = date('U')+50;
 			$update = mysql_query("UPDATE `users` SET `online` = '".$time."' WHERE `id` = '".$_SESSION['uid']."'");
-			
+
 			?>
+
+            <div class="navbar navbar-default" id="navbar">
+            <script type="text/javascript">
+                try{ace.settings.check('navbar' , 'fixed')}catch(e){}
+            </script>
+
+            <div class="navbar-container" id="navbar-container">
+                <div class="navbar-header pull-left">
+                    <a href="#" class="navbar-brand">
+                        <small>
+                            <i class="icon-leaf"></i>
+                            Hangar
+                        </small>
+                    </a><!-- /.brand -->
+                </div><!-- /.navbar-header -->
+
+                <div class="navbar-header pull-right" role="navigation">
+                    <ul class="nav ace-nav">
+                        <li class="light-blue">
+                            <a data-toggle="dropdown" href="#" class="dropdown-toggle">
+                                <img class="nav-user-photo" src="../assets/avatars/user.jpg" alt="Jason's Photo" />
+                                <span class="user-info">
+                                    <small>Welcome,</small>
+                                    <?php echo $row['name'].' '.$row['lastname']; ?>
+                                </span>
+
+                                <i class="icon-caret-down"></i>
+                            </a>
+
+                            <ul class="user-menu pull-right dropdown-menu dropdown-yellow dropdown-caret dropdown-close">
+                                <li>
+                                    <a href="#">
+                                        <i class="icon-cog"></i>
+                                        Settings
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href="#">
+                                        <i class="icon-user"></i>
+                                        Profile
+                                    </a>
+                                </li>
+
+                                <li class="divider"></li>
+
+                                <li>
+                                    <a href="../../logout.php">
+                                        <i class="icon-off"></i>
+                                        Logout
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul><!-- /.ace-nav -->
+                </div><!-- /.navbar-header -->
+            </div><!-- /.container -->
+        </div>
+
+        <div class="main-container" id="main-container">
+            <div class="main-container-inner">
+                <a class="menu-toggler" id="menu-toggler" href="#">
+                    <span class="menu-text"></span>
+                </a>
+                <div class="sidebar" id="sidebar">
+                    <script type="text/javascript">
+                        try{ace.settings.check('sidebar' , 'fixed')}catch(e){}
+                    </script>
+
+                    <div class="sidebar-shortcuts" id="sidebar-shortcuts">
+                        <div class="sidebar-shortcuts-large" id="sidebar-shortcuts-large">
+                            <button class="btn btn-success">
+                                <i class="icon-signal"></i>
+                            </button>
+
+                            <button class="btn btn-info">
+                                <i class="icon-pencil"></i>
+                            </button>
+
+                            <button class="btn btn-warning">
+                                <i class="icon-group"></i>
+                            </button>
+
+                            <button class="btn btn-danger">
+                                <i class="icon-cogs"></i>
+                            </button>
+                        </div>
+
+                        <div class="sidebar-shortcuts-mini" id="sidebar-shortcuts-mini">
+                            <span class="btn btn-success"></span>
+
+                            <span class="btn btn-info"></span>
+
+                            <span class="btn btn-warning"></span>
+
+                            <span class="btn btn-danger"></span>
+                        </div>
+                    </div><!-- #sidebar-shortcuts -->
+
+                    <ul class="nav nav-list">
+                        <li class="active">
+                            <a href="#">
+                                <i class="icon-user"></i>
+                                <span class="menu-text"> My Profile </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="timelines-to-me.php">
+                                <i class="icon-sitemap"></i>
+                                <span class="menu-text"> Timelines added to me </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="add-timeline.php">
+                                <i class="icon-plus"></i>
+                                <span class="menu-text"> Add user to project </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="../../logout.php">
+                                <i class="icon-mail-reply"></i>
+                                <span class="menu-text"> Log Out </span>
+                            </a>
+                        </li>
+                    </ul><!-- /.nav-list -->
+
+                    <div class="sidebar-collapse" id="sidebar-collapse">
+                        <i class="icon-double-angle-left" data-icon1="icon-double-angle-left" data-icon2="icon-double-angle-right"></i>
+                    </div>
+
+                    <script type="text/javascript">
+                        try{ace.settings.check('sidebar' , 'collapsed')}catch(e){}
+                    </script>
+                </div>
+                <div class="main-content">
+                    <div class="breadcrumbs" id="breadcrumbs">
+                        <script type="text/javascript">
+                            try{ace.settings.check('breadcrumbs' , 'fixed')}catch(e){}
+                        </script>
+
+                        <ul class="breadcrumb">
+                            <li>
+                                <i class="icon-home home-icon"></i>
+                                <a href="#">Home</a>
+                            </li>
+                            <li class="active">Dashboard</li>
+                        </ul><!-- .breadcrumb -->
+
+                        <div class="nav-search" id="nav-search">
+                            <form class="form-search">
+                                <span class="input-icon">
+                                    <input type="text" placeholder="Search ..." class="nav-search-input" id="nav-search-input" autocomplete="off" />
+                                    <i class="icon-search nav-search-icon"></i>
+                                </span>
+                            </form>
+                        </div><!-- #nav-search -->
+                    </div>
+                                <div class="page-content">
+                                    <div class="page-header">
+                                        <h1>
+                                            PM Dashboard
+                                            <small>
+                                                <i class="icon-double-angle-right"></i>
+                                                adding timeline
+                                            </small>
+                                        </h1>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-xs-12">
+                                    <div class="row">
+                                        <div class="col-sm-12">
+
+                                        </div><!-- /span -->
+                                    </div><!-- /row -->
+                                </div>
+                            </div>
+                        </div>
+                     </div>
+            </div>
+        </div>
+
+
+
+
+
+
 	<h1>Adding Timeline</h1>
 		<a href="pm-panel.php">PM Panel</a>
 		<h3>Your project is: <?php echo $prjctName; ?> </h3>
-		<p> <?php echo $prjctText; ?> </br> <?php echo $prjctStartDate; ?> </p>	
+		<p> <?php echo $prjctText; ?> </br> <?php echo $prjctStartDate; ?> </p>
 		<form action="../../controllers/addtimeline.php" method="post">
 		<input type="hidden" id="idFromProject" name="idFromProject" value="<?php echo $idproject; ?>" readonly></input>
 		<p>Timeline Start Date:<input type="text" placeholder="format yyyy,mm,dd" id="startDate" name="startDate"></input></p>
@@ -88,11 +323,59 @@ function clean($str)
 		</form>
 
 
+       <!-- basic scripts -->
+
+        <!--[if !IE]> -->
+
+        <script type="text/javascript">
+            window.jQuery || document.write("<script src='../assets/js/jquery-2.0.3.min.js'>"+"<"+"/script>");
+        </script>
+
+        <!-- <![endif]-->
+
+        <!--[if IE]>
+            <script type="text/javascript">
+                window.jQuery || document.write("<script src='assets/js/jquery-1.10.2.min.js'>"+"<"+"/script>");
+            </script>
+        <![endif]-->
+
+        <script type="text/javascript">
+            if("ontouchend" in document) document.write("<script src='../assets/js/jquery.mobile.custom.min.js'>"+"<"+"/script>");
+        </script>
+        <script src="../assets/js/bootstrap.min.js"></script>
+        <script src="../assets/js/typeahead-bs2.min.js"></script>
+
+        <!-- page specific plugin scripts -->
+
+        <!--[if lte IE 8]>
+          <script src="assets/js/excanvas.min.js"></script>
+        <![endif]-->
+
+        <script src="../assets/js/jquery-ui-1.10.3.custom.min.js"></script>
+        <script src="../assets/js/jquery.ui.touch-punch.min.js"></script>
+        <script src="../assets/js/chosen.jquery.min.js"></script>
+        <script src="../assets/js/fuelux/fuelux.spinner.min.js"></script>
+        <script src="../assets/js/date-time/bootstrap-datepicker.min.js"></script>
+        <script src="../assets/js/date-time/bootstrap-timepicker.min.js"></script>
+        <script src="../assets/js/date-time/moment.min.js"></script>
+        <script src="../assets/js/date-time/daterangepicker.min.js"></script>
+        <script src="../assets/js/bootstrap-colorpicker.min.js"></script>
+        <script src="../assets/js/jquery.knob.min.js"></script>
+        <script src="../assets/js/jquery.autosize.min.js"></script>
+        <script src="../assets/js/jquery.inputlimiter.1.3.1.min.js"></script>
+        <script src="../assets/js/jquery.maskedinput.min.js"></script>
+        <script src="../assets/js/bootstrap-tag.min.js"></script>
+
+        <!-- ace scripts -->
+
+        <script src="../assets/js/ace-elements.min.js"></script>
+        <script src="../assets/js/ace.min.js"></script>
+
 <?php
-		
+
 		//make sure you close the check if their online
 		}
-		
+
 		?>
 </body>
 </html>
--- logout.php
+++ logout.php
@@ -12,29 +12,81 @@ include "config/functions.php";
 <html>
 	<head>
 		<title>Login with Users Online Tutorial</title>
+        <script src="//localhost:35729/livereload.js"></script>
 		<link rel="stylesheet" type="text/css" href="style.css" />
+
+         <!-- basic styles -->
+
+        <link href="app/assets/css/bootstrap.min.css" rel="stylesheet" />
+        <link rel="stylesheet" href="app/assets/css/font-awesome.min.css" />
+
+
+
+        <!-- fonts -->
+
+        <link rel="stylesheet" href="app/assets/css/ace-fonts.css" />
+
+        <!-- ace styles -->
+
+        <link rel="stylesheet" href="app/assets/css/ace.min.css" />
+        <link rel="stylesheet" href="app/assets/css/ace-rtl.min.css" />
+
+        <!-- ace js -->
+        <script src='app/assets/js/jquery-2.0.3.min.js'></script>
+        <script src="app/assets/js/bootstrap.min.js"></script>
+        <script src="app/assets/js/typeahead-bs2.min.js"></script>
 	</head>
 	<body>
-		<center><a href="./">Index</a></center>
+		<!-- <center><a href="./">Index</a></center> -->
 		<?php
-		
+
 		//check if the login session does no exist
 		if(!$_SESSION['uid']){
 			//if it doesn't display an error message
-			echo "<center>You need to be logged in to log out!</center>";
+			/*echo "<center>You need to be logged in to log out!</center>";*/
+            echo "<div class='row'>
+                     <div class='col-sm-4 col-sm-offset-4'>
+                        <div class='alert alert-block alert-warning'>
+                            <p>
+                                <strong>
+                                    <i class='icon-ok'></i>
+                                </strong>
+                                    You need to be logged in to log out!
+                            </p>
+                            <p>
+                                <a href='./' class='btn btn-sm btn-warning btn-block'>Go to Index</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>";
 		}else{
 			//if it does continue checking
-			
+
 			//update to set this users online field to the current time
 			mysql_query("UPDATE `users` SET `online` = '".date('U')."' WHERE `id` = '".$_SESSION['uid']."'");
-			
+
 			//destroy all sessions canceling the login session
 			session_destroy();
-			
+
 			//display success message
-			echo "<center>You have successfully logged out!</center>";
+			/*echo "<center>You have successfully logged out!</center>";*/
+            echo            "<div class='row'>
+                                 <div class='col-sm-4 col-sm-offset-4'>
+                                    <div class='alert alert-block alert-success'>
+                                        <p>
+                                            <strong>
+                                                <i class='icon-ok'></i>
+                                            </strong>
+                                                You have successfully logged out!
+                                        </p>
+                                        <p>
+                                            <a href='./' class='btn btn-sm btn-success btn-block'>Go to Index</a>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>";
 		}
-		
+
 		?>
 	</body>
 </html>
