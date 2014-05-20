d84a67a0442ab8ad6047e2a8de4dc5b1285f4352
Mauricio Ramirez <mau.ram.ro@gmail.com>
2014-05-20 15:49:43 -0600

    pm & color update

--- app/pm/timelines/timeline.php
+++ app/pm/timelines/timeline.php
@@ -74,7 +74,7 @@ $json_id = $idProject;
                     <ul class="nav ace-nav">
                         <li class="light-blue">
                             <a data-toggle="dropdown" href="#" class="dropdown-toggle">
-                                <img class="nav-user-photo" src="../../assets/avatars/user.jpg" alt="Jason's Photo" />
+                               <!--  <img class="nav-user-photo" src="../../assets/avatars/user.jpg" alt="Jason's Photo" /> -->
                                 <span class="user-info">
                                     <small>Welcome,</small>
                                     <?php echo $row['name'].' '.$row['lastname']; ?>
