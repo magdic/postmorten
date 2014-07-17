5fcf97b5074cc6a5d0d2bbcd3e480ea01a191e4d
Magdiel <luisc@thehangar.cr>
2014-07-17 15:58:21 -0600

    The same issue, working on wysiwyg PM

--- app/lead/down.php
+++ app/lead/down.php
@@ -3,19 +3,35 @@
             </div>
         </div>
                 
-        <script src='../assets/js/jquery.mobile.custom.min.js'></script>
+        <script type="text/javascript">
+            if("ontouchend" in document) document.write("<script src='../assets/js/jquery.mobile.custom.min.js'>"+"<"+"/script>");
+        </script>
         <script src="../assets/js/bootstrap.min.js"></script>
         <script src="../assets/js/typeahead-bs2.min.js"></script>
 
         <!-- page specific plugin scripts -->
-        
-        <script src="../assets/js/bootstrap-tag.min.js"></script>
-        <script src="../assets/js/jquery.hotkeys.min.js"></script>
-        <script src="../assets/js/bootstrap-wysiwyg.min.js"></script>
+
+        <!--[if lte IE 8]>
+          <script src="assets/js/excanvas.min.js"></script>
+        <![endif]-->
+
         <script src="../assets/js/jquery-ui-1.10.3.custom.min.js"></script>
         <script src="../assets/js/jquery.ui.touch-punch.min.js"></script>
+        <script src="../assets/js/chosen.jquery.min.js"></script>
+        <script src="../assets/js/fuelux/fuelux.spinner.min.js"></script>
+        <script src="../assets/js/date-time/bootstrap-datepicker.min.js"></script>
+        <script src="../assets/js/date-time/bootstrap-timepicker.min.js"></script>
+        <script src="../assets/js/date-time/moment.min.js"></script>
+        <script src="../assets/js/date-time/daterangepicker.min.js"></script>
+        <script src="../assets/js/jquery.autosize.min.js"></script>
+        <script src="../assets/js/jquery.inputlimiter.1.3.1.min.js"></script>
+        <script src="../assets/js/jquery.maskedinput.min.js"></script>
+        <script src="../assets/js/bootstrap-tag.min.js"></script>
+        <script src="../assets/js/markdown/markdown.min.js"></script>
+        <script src="../assets/js/markdown/bootstrap-markdown.min.js"></script>
+        <script src="../assets/js/jquery.hotkeys.min.js"></script>
+        <script src="../assets/js/bootstrap-wysiwyg.min.js"></script>
         <script src="../assets/js/bootbox.min.js"></script>
-        <script src="../assets/js/jquery.slimscroll.min.js"></script>
 
         <!-- ace scripts -->
 
--- app/lead/lead-add-project.php
+++ app/lead/lead-add-project.php
@@ -332,13 +332,6 @@ function clean($str)
                     }
                 });
 
-                // $(".chosen-select").chosen();
-                // $('#chosen-multiple-style').on('click', function(e){
-                //     var target = $(e.target).find('input[type=radio]');
-                //     var which = parseInt(target.val());
-                //     if(which == 2) $('#form-field-select-4').addClass('tag-input-style');
-                //      else $('#form-field-select-4').removeClass('tag-input-style');
-                // });
 
 
 
